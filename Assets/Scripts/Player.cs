2bd12c6da0af0ff1620aec372c78d9308b559d82
Kelli Porter <77407178+portekn@users.noreply.github.com>
2022-09-16 12:59:31 -0400

    Players are Synced
    Player is synced across the network and is controlled through the server.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,4 +2,31 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class Player : NetworkBehaviour {
+
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
+    public float movementSpeed = 1.0f;
+
+    Vector3 CalcMovement(){
+        Vector3 moveVect = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveVect *= movementSpeed;
+        return moveVect;
+    }
+
+    [ServerRpc]
+    void RequestPositionForMovementServerRpc(Vector3 movement){
+        Position.Value += movement;
+    }
+
+    private void Update(){
+        if (IsOwner){
+            Vector3 move = CalcMovement();
+            if(move.magnitude > 0){
+                RequestPositionForMovementServerRpc(move);
+            }
+        }
+        else {
+            transform.position = Position.Value;
+        }
+    }
 }
